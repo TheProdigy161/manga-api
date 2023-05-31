50351a385c63e386fea11edbb91f481ce2ef80c3
TheProdigy161 <TheProdigy161@yahoo.co.uk>
2023-05-31 22:19:07 +0100

    Initial webapi commit

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangaApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    private readonly ILogger<HealthController> _logger;
+
+    public HealthController(ILogger<HealthController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
