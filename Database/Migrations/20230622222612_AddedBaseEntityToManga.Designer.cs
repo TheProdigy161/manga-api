b643bf417184ff0bc53a505b25e06057cc0e3211
Ross Bratton <rossbratton20@gmail.com>
2023-06-22 23:27:10 +0100

    Added migration for BaseEntity

--- Database/Migrations/20230622222612_AddedBaseEntityToManga.Designer.cs
+++ Database/Migrations/20230622222612_AddedBaseEntityToManga.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MangaApi.Database.Migrations
 {
     [DbContext(typeof(MangaContext))]
-    partial class MangaContextModelSnapshot : ModelSnapshot
+    [Migration("20230622222612_AddedBaseEntityToManga")]
+    partial class AddedBaseEntityToManga
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
