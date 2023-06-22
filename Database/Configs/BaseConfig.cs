1996226ca1c3050b80ea84b9c5cbd64603bc2765
Ross Bratton <rossbratton20@gmail.com>
2023-06-22 23:40:33 +0100

    Updated to ensure that CreatedDate is populated on add

--- Database/Configs/BaseConfig.cs
+++ Database/Configs/BaseConfig.cs
@@ -5,7 +5,7 @@ public static class BaseEntityConfig
 {
     public static void BaseEntity<T>(this EntityTypeBuilder<T> entity) where T : BaseEntity
     {
-        entity.Property(x => x.CreatedDate).IsRequired();
+        entity.Property(x => x.CreatedDate).HasDefaultValueSql("now()").IsRequired();
         entity.Property(x => x.UpdatedDate);
         entity.Property(x => x.DeletedDate);
         entity.Property(x => x.Deleted);
