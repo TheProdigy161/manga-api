84d2c25188312794bd2d1c6c3801c215f044b56d
Ross Bratton <rossbratton20@gmail.com>
2023-07-30 10:32:46 +0100

    Updated seeder

--- Database/Seeding/DataSeeder.cs
+++ Database/Seeding/DataSeeder.cs
@@ -6,14 +6,15 @@ public static class DataSeeder
     public static void Seed(this ModelBuilder modelBuilder)
     {
         string mangaJson = File.ReadAllText("Database\\Seeding\\Data\\MangaData.json");
-        List<MangaCreateDto>? mangas = JsonConvert.DeserializeObject<List<MangaCreateDto>>(mangaJson);
+        List<Manga>? mangas = JsonConvert.DeserializeObject<List<Manga>>(mangaJson);
 
         if (mangas is not null)
         {
-            foreach (MangaCreateDto manga in mangas)
+            foreach (Manga manga in mangas)
             {
                 modelBuilder.Entity<Manga>().HasData(new Manga
                 {
+                    Id = manga.Id,
                     Name = manga.Name,
                     ImageUrl = manga.ImageUrl,
                     ReleaseDate = manga.ReleaseDate,
