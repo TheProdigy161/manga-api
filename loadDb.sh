589599a51fabde0c15f9d848c6d09cd14a55095f
TheProdigy161 <rossbratton20@gmail.com>
2025-03-05 22:17:14 +0000

    Updated appsettings.Development.json to work with docker container database

---
+++ loadDb.sh
@@ -0,0 +1,22 @@
+CONTAINER_NAME='manga-db'
+PASSWORD='Password123!'
+
+{
+    # Run the postgres container
+    docker start $CONTAINER_NAME 2> /dev/null;
+
+} || {
+
+    # Create the postgres container
+    echo "Container not found, creating a new one";
+
+    docker run -d \
+        -e "ACCEPT_EULA=Y" \
+        -e "SA_PASSWORD=$PASSWORD" \
+        -p 1433:1433 \
+        --name $CONTAINER_NAME \
+        mcr.microsoft.com/mssql/server;
+}
+
+echo "Container started!";
+
