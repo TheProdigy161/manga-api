f133558846a40dd0650ec9a838c6b9ada330ead1
TheProdigy161 <rossbratton20@gmail.com>
2025-03-08 08:27:15 +0000

    Updated to ensure that local db is started before debugging

--- loadDb.sh
+++ loadDb.sh
@@ -3,6 +3,7 @@ PASSWORD='Password123!'
 
 {
     # Run the postgres container
+    echo "Starting container";
     docker start $CONTAINER_NAME 2> /dev/null;
 
 } || {
@@ -17,5 +18,9 @@ PASSWORD='Password123!'
         --name $CONTAINER_NAME \
         mcr.microsoft.com/mssql/server;
 }
+
+# Wait for 5 seconds to allow for container to start
+echo "Waiting for container to start";
+sleep 5
 
 echo "Container started!";
